e6ed92e40483f61a4dba95575883de60a9f2fa9d
Roshan Patel <roshanpatel12309@gmail.com>
2025-12-29 15:30:56 +0530

    Modified UI

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,27 +1,38 @@
-import React, { useContext } from 'react'
-import { assets } from '../assets/assets'
-import { AppContent } from '../context/AppContext'
+import React, { useContext } from "react";
+import { assets } from "../assets/assets";
+import { AppContent } from "../context/AppContext";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
-
-  const { userData } = useContext(AppContent)
+  const { userData } = useContext(AppContent);
+  const navigate = useNavigate();
 
   return (
-    <div className='flex flex-col items-center m-20 px-4 text-center text-grey-800'>
+    <div className="flex flex-col items-center m-20 px-4 text-center text-grey-800">
       <div className="w-70 h-66 rounded-full flex justify-center items-center overflow-hidden">
-        <img src={assets.header_img} alt="" className='w-50 h-40' />
-        </div>
-        <h1 className='flex items-center gap-2 text-xl sm:text-3xl font-medium mb-2'>Hey {userData ? userData.user.name : 'Vendor'}!
-          {/* <img className='w-8 aspect-square' src={assets.hand_wave} alt='' /> */}
-        </h1>
+        <img src={assets.header_img} alt="" className="w-50 h-40" />
+      </div>
+      <h1 className="flex items-center gap-2 text-xl sm:text-3xl font-medium mb-2">
+        Hey {userData ? userData.name : "Vendor"}!
+        {/* <img className='w-8 aspect-square' src={assets.hand_wave} alt='' /> */}
+      </h1>
 
-        <h2 className='text-3xl sm:text-5xl  font-semibold mb-4'>Welcome To Decathlon</h2>
+      <h2 className="text-3xl sm:text-5xl  font-semibold mb-4">
+        Welcome Back To Decathlon
+      </h2>
 
-        <p className='mb-8 max-w-md'>SPORT FOR ALL - ALL FOR SPORTS</p>
+      <p className="mb-8 max-w-md">SPORT FOR ALL - ALL FOR SPORTS</p>
 
-        <button className='border border-grey-500 rounded-full px-8 py-2.5 hover:bg-gray-100 transition-all'>Get Started</button>
+      {userData && (
+        <button
+          onClick={() => navigate("/dashboard")}
+          className="border border-grey-500 rounded-full px-8 py-2.5 hover:bg-gray-100 transition-all cursor-pointer"
+        >
+          Get Started
+        </button>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
