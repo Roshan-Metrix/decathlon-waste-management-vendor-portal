4aacbd520976b0ffe3c4f23cccf66bb0fbb916f8
Roshan Patel <roshanpatel12309@gmail.com>
2026-03-04 14:21:03 +0530

    Added TimeStamp function

--- frontend/lib/Helper.js
+++ frontend/lib/Helper.js
@@ -1,8 +1,19 @@
-export const formatDate = (date) =>
-    new Date(date).toLocaleString("en-US", {
+export const formatDate = (date) => {
+  if (!date) return "N/A";
+    return new Date(date).toLocaleString("en-US", {
       year: "numeric",
       month: "short",
       day: "numeric",
       hour: "2-digit",
       minute: "2-digit",
  });
+};
+
+export const formatTime = (date) => {
+  if (!date) return "N/A";
+    return new Date(date).toLocaleTimeString("en-US", {
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+    });
+};
