51866d4dc358773379519158741edd887d57198e
Roshan Patel <roshanpatel12309@gmail.com>
2026-08-21 14:22:12 +0530

    feat: manage vendor feature

---
+++ frontend/src/pages/adminPages/vendorsManage/EditVendors.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import NavBar from '../../../components/NavBar'
+
+const EditVendors = () => {
+  return (
+    <div className="flex flex-col items-center m-20 px-4 text-gray-800">
+      <NavBar />
+    </div>
+  )
+}
+
+export default EditVendors
